f18be8c43fd6ecfd548c64e6673fe650792769b1
youyeongjin <show5116@naver.com>
2022-04-29 13:32:43 +0900

    FEAT : Add CLeanUp Function

---
+++ learn-react/src/cleanUp.js
@@ -0,0 +1,26 @@
+import {useEffect, useState} from "react";
+
+function Hello(){
+    // showing 할 떄 마다 useEffect 내부 함수가 실행되고,
+    // Component 가 삭제될 때 return 문이 실행된다.
+
+    useEffect(()=>{
+        console.log("hello created");
+        return () => console.log("hello destroyed");
+    },[]);
+
+    return <h1>Hello</h1>;
+}
+
+function App() {
+    const [showing, setShowing] = useState(false);
+    const onClick = () => setShowing((prev) => !prev);
+    return (
+        <div>
+            {showing ? <Hello /> : null}
+            <button onClick={onClick}>{showing ? "Hide" : "Show"}</button>
+        </div>
+    );
+}
+
+export default App;
